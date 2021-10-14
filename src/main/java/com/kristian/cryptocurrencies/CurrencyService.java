0fc9a319260f5b0106d9b8d1c2598463831c8310
Kristian <kristian.gollasch@wp.pl>
2021-10-14 17:16:29 +0200

    API Key is loaded from properties

--- src/main/java/com/kristian/cryptocurrencies/CurrencyService.java
+++ src/main/java/com/kristian/cryptocurrencies/CurrencyService.java
@@ -3,6 +3,7 @@ package com.kristian.cryptocurrencies;
 import com.kristian.cryptocurrencies.dto.Currency;
 import com.kristian.cryptocurrencies.dto.ExchangeRequest;
 import com.kristian.cryptocurrencies.dto.Root;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,14 +15,17 @@ import static com.kristian.cryptocurrencies.Util.getPriceFromJson;
 import static com.kristian.cryptocurrencies.Util.sendRequestToApiProvider;
 
 @Service
+@ConfigurationProperties("integration")
 public class CurrencyService {
 
-    private final String URI = "https://api.nomics.com/v1/currencies/ticker?key=f351ff5711407ffbbbdba7c7456e243543302219&ids=";
+    private final String URI = "https://api.nomics.com/v1/currencies/ticker?key=";
+
+    private String apiKey;
 
     public Map<String, Double> getQuotationList(String baseCurrency, String filter) {
         Map<String, Double> currenciesWithPrices = new HashMap<>();
         for (String currency : filter.split(",")) {
-            final String uri = URI + baseCurrency + "&convert=" + currency;
+            final String uri = URI + apiKey + "&ids=" + baseCurrency + "&convert=" + currency;
             String jsonResponse = sendRequestToApiProvider(uri);
             currenciesWithPrices.put(currency, getPriceFromJson(jsonResponse));
         }
@@ -31,10 +35,20 @@ public class CurrencyService {
     public Root getCryptocurrencyExchangeForecast(ExchangeRequest exchangeRequest) {
         List<Currency> currencies = new ArrayList<>();
         for (String to : exchangeRequest.getTo()) {
-            String uri = URI + exchangeRequest.getFrom() + "&interval=1d&convert=" + to;
+            String uri = URI + apiKey + "&ids=" + exchangeRequest.getFrom() + "&interval=1d&convert=" + to;
             String response = sendRequestToApiProvider(uri);
             currencies.add(new Currency.CurrencyBuilder().currency(to).rate(getPriceFromJson(response)).amount(exchangeRequest.getAmount()).result(exchangeRequest.getAmount() * getPriceFromJson(response)).fee(0).build());
         }
         return new Root(exchangeRequest.getFrom(), currencies);
     }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+
 }
